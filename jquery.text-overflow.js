8843d5c6b946786c8d9bc273ed058cc8e84707ee
bs@cvt121.cvt.dk <bs@cvt121.cvt.dk>
2009-08-14 12:08:52 +0200

    Some small fixes and an optimization when there is only a single text node.

--- jquery.text-overflow.js
+++ jquery.text-overflow.js
@@ -1,5 +1,5 @@
 /*!
- * jQuery Text Overflow v0.52
+ * jQuery Text Overflow v0.62
  *
  * Licensed under the new BSD License.
  * Copyright 2009, Bram Stein
@@ -7,33 +7,36 @@
  */
 /*global jQuery, document, setInterval*/
 (function ($) {
-	$.fn.textNodes = function() {
-		var ret = [];
-		$.each(this[0].childNodes, function() {
-      		if (this.nodeType == 3) {
-				ret.push(this);
-			} else {
-				$.each(this.childNodes, arguments.callee);
-			}
-		});
-		return $(ret);
-	};
+	var hasRange = document.createRange !== undefined,
 
-	var style = document.documentElement.style,
-
-		hasTextOverflow = ('textOverflow' in style || 'OTextOverflow' in style),
-		hasRange = document.createRange !== undefined,
+        getTextNodes = function (element) {
+            var result = [];
+            $.each(element[0].childNodes, function () {
+                if (this.nodeType === 3) {
+                    result.push(this);
+                } else {
+                    $.each(this.childNodes, arguments.callee);
+                }
+            });
+            return $(result);
+        },
 
 		htmlSubstr = function (nodes, start, end) {
 			var range = document.createRange(),
 		        i = 0, len = nodes.length, index = 0, node;                   
 
 		    if (end <= start) {
-		        return element;
+		        return $(nodes);
 		    }
-
-		    // TODO: perhaps add some check here if there is only 
-		    // one text node and then use a normal substr (or if DOM Ranges are not supported.)
+            
+            if (len === 1 || !hasRange) {
+                // If ranges are not supported, or there is 
+                // only one text node we use the normal 
+                // substr method as it is much faster.
+                return $.map(nodes, function (e) {
+                    return e.textContent;
+                }).join('').substr(start, end);
+            }
 		        
 		    for (; i < len; i += 1) {
 		        node = nodes[i];
@@ -57,57 +60,53 @@
         textOverflow: function (str, autoUpdate) {
             var more = str || '…';
             
-            if (!hasTextOverflow) {
-                return this.each(function () {
-                    var element = $(this),
+            return this.each(function () {
+                var element = $(this),
 
-						// the clone element we modify to measure the width 
-                        clone = element.clone(),
+                    // the clone element we modify to measure the width 
+                    clone = element.clone(),
 
-						// we safe a copy so we can restore it if necessary
-                        originalElement = element.clone(),
+                    // we safe a copy so we can restore it if necessary
+                    originalElement = element.clone(),
 
-                        originalText = element.text(),
-                        originalWidth = element.width(),
-						textNodes = originalElement.textNodes(),
-                        low = 0, mid = 0,
-                        high = originalText.length,
-                        reflow = function () {
-                            if (originalWidth !== element.width()) {
-								element.replaceWith(originalElement);
-								element = originalElement;
-								originalElement = element.clone();
-                            	element.textOverflow(str, false);
-								originalWidth = element.width();								
-                            }
-                        };
+                    originalText = element.text(),
+                    originalWidth = element.width(),
+                    textNodes = getTextNodes(originalElement),
+                    low = 0, mid = 0,
+                    high = originalText.length,
+                    reflow = function () {
+                        if (originalWidth !== element.width()) {
+                            element.replaceWith(originalElement);
+                            element = originalElement;
+                            originalElement = element.clone();
+                            element.textOverflow(str, false);
+                            originalWidth = element.width();								
+                        }
+                    };
 
-                    element.after(clone.hide());
+                element.after(clone.hide());
+            
+                if (clone.width() > originalWidth) {
+                    while (low < high) {
+                        mid = Math.floor(low + ((high - low) / 2));
+                        clone.empty().append(htmlSubstr(textNodes, 0, mid)).append(more);
+                        if (clone.width() < originalWidth) {
+                            low = mid + 1;
+                        } else {
+                            high = mid;
+                        }
+                    }
+
+                    if (low < originalText.length) {
+                        element.empty().append(htmlSubstr(textNodes, 0, low - 1)).append(more);
+                    }
+                }
+                clone.remove();
                 
-                    if (clone.width() > originalWidth) {
-                        while (low < high) {
-                            mid = Math.floor(low + ((high - low) / 2));
-                            clone.empty().append(htmlSubstr(textNodes, 0, mid)).append(more);
-                            if (clone.width() < originalWidth) {
-                                low = mid + 1;
-                            } else {
-                                high = mid;
-                            }
-                        }
-  
-                        if (low < originalText.length) {
-                            element.empty().append(htmlSubstr(textNodes, 0, low - 1)).append(more);
-                        }
-                    }
-                    clone.remove();
-                    
-                    if (autoUpdate) {    
-                        setInterval(reflow, 200);
-                    }
-                });
-            } else {
-                return this;
-            }
+                if (autoUpdate) {    
+                    setInterval(reflow, 200);
+                }
+            });
         }
 	});
 })(jQuery);
